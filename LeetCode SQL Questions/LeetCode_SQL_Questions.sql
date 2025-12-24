cbd5ac363eec8910f16509ae548b61453df25b7b
Bappy-kumar <bappykumar4221@gmail.com>
2025-12-24 23:59:26 +0545

    Updated files

--- LeetCode SQL Questions/LeetCode_SQL_Questions.sql
+++ LeetCode SQL Questions/LeetCode_SQL_Questions.sql
@@ -33,3 +33,26 @@ insert into Address (addressId, personId, city, state) values ('2', '3', 'Leetco
 
 select p.firstName, p.lastName, a.city, a.state from Person as p left join Address as a
 on p.personId = a.personId;
+
+-- --------------------------------------------------------------------------------------------------------------------
+drop table Employee;
+Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int);
+Truncate table Employee;
+insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3');
+insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4');
+insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', NULL);
+insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', NULL);
+
+select * from employee;
+
+-- Write a solution to find the employees who earn more than their managers.
+--    Return the result table in any order.
+
+SELECT name as Employee
+FROM Employee e
+WHERE salary > (
+    SELECT salary
+    FROM Employee
+    WHERE id = e.managerId
+);
+
